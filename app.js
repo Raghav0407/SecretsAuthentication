8bea07c642796b4bd72629c118f407b00032092c
Raghav0407 <90747311+Raghav0407@users.noreply.github.com>
2022-07-16 18:55:41 +0530

    Commit

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+// const encrypt=require("mongoose-encryption");
+const md5 =require("md5");
+
 
 
 
 
 const app=express();
- 
+console.log(md5("123456"));
 console.log(process.env.API_KEY);
 app.use(express.static("public"));
 
@@ -26,7 +28,7 @@ const userSchema=new mongoose.Schema({
     password:String
 })
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET , encryptedFields:["password"]});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET , encryptedFields:["password"]});
 
 
 
@@ -52,7 +54,7 @@ app.post("/register",function(req,res)
 {
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     })
     newUser.save(function(err)
     {
@@ -70,7 +72,7 @@ app.post("/register",function(req,res)
 app.post("/login",function(req,res)
 {
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password)  ;
     User.findOne({email:username},function(err,foundUser)
     {
         if(err)
